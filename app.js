3e8da1d2fbeb8e56ceea95165d1fcff2350bc5e2
jahfredW <fred.gruwe@gmail.com>
2022-08-22 13:37:50 +0200

    convention de réponses

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const f = require('functions')
 const app = express()
 
 /*middleware : fonction qui va être lue au moment où il y a une requête,
@@ -25,21 +26,34 @@ app.use(morgan('dev'))
 
 // récupération via :id
 app.get('/api/v1/members/:id', (req, res) => {
-    res.send(members[(req.params.id) - 1]['name'])
+    if (req.params.id <= members.length) {
+        res.json(f.success(members[req.params.id - 1].name));
+    } else {
+        res.json(f.error('out of range'));
+    }
 })
 
 // récupération de tous les membres, utilisation de query -> correspond à url ?<data>
 app.get('/api/v1/members', (req, res) => {
     if (req.query.max !== undefined && req.query.max > 0) {
-        res.send(members.slice(0, req.query.max))
+        res.json(f.success(members.slice(0, req.query.max)))
         // slice renvoie une portion de l'array, entre indice et indice -1
+    } else if (req.query.max !== undefined) {
+        /*  arr.slice()
+            arr.slice(début)
+               arr.slice(début, fin)*/
+        res.json(f.error('Wrong max value'));
     }
-    /*  arr.slice()
-        arr.slice(début)
-           arr.slice(début, fin)*/
-    res.send(members)
+    else {
+        /*  arr.slice()
+            arr.slice(début)
+               arr.slice(début, fin)*/
+        res.json(f.success(members));
+    }
 })
-
 
 app.listen(8080, () => console.log('Started on port 8080')
 );
+
+
+
