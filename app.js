82b33e359e54d41ef5b0a26a700ca51a6fe180ac
jahfredW <fred.gruwe@gmail.com>
2022-08-22 09:59:54 +0200

    params des urls

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const app = express()
 
+
+
 app.get('/api', (req, res) => {
-    res.send('Root API')
+    res.send('Root API');
 })
 
 app.get('/api/v1', (req, res) => {
-    res.send("API version 1")
+    res.send("API version 1");
+})
+
+
+/* paramètres des urls : */
+/* :<param> */
+app.get('/api/v1/books/:id/:id2', (req, res) => {
+    res.send(req.params);
 })
 
 
 app.listen(8080, () => console.log('Started on port 8080')
-)
+);
